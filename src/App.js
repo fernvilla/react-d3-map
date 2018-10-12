25566494d2a21f1a05f11536572a8ab399eb00d9
Fernando Villasenor <fvilla18@gmail.com>
2018-10-11 21:49:43 -0700

    Set basic us map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { queue } from "d3-queue";
+// import { geo } from "d3-geo";
+// import { scale } from "d3-scale";
+import * as d3 from "d3";
+// import * as topojson from "topojson";
+import states from "./us-states.json";
+import "./App.css";
 
-class App extends Component {
+class USMap extends React.Component {
+  componentDidMount() {
+    const svg = d3.select(this.anchor);
+    const { width, height, setSelectedState } = this.props;
+
+    const projection = d3
+      .geoAlbersUsa()
+      .scale(1280)
+      .translate([width / 2, height / 2]);
+
+    const path = d3.geoPath(projection);
+
+    svg
+      .selectAll("path")
+      .data(states.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("stroke", "#fff")
+      .style("stroke-width", "1")
+      .style("fill", "#00b9ad")
+      .on("click", d => setSelectedState(d.properties.name));
+  }
+
   render() {
+    return <g ref={anchor => (this.anchor = anchor)} />;
+  }
+}
+
+class App extends React.Component {
+  state = {
+    selectedState: null
+  };
+
+  setSelectedState = state => {
+    this.setState({ selectedState: state });
+  };
+
+  render() {
+    console.log(this.state.selectedState);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="map">
+        <svg width="960" height="600">
+          <USMap
+            width={960}
+            height={600}
+            setSelectedState={this.setSelectedState}
+          />
+        </svg>
       </div>
     );
   }
