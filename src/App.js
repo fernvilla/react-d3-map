b5a2c1f734b15f0168262cb5c4bb489710c334d1
Fernando Villasenor <fvilla18@gmail.com>
2018-10-11 23:27:38 -0700

    More tests

--- src/App.js
+++ src/App.js
@@ -3,20 +3,67 @@ import React from "react";
 // import { geo } from "d3-geo";
 // import { scale } from "d3-scale";
 import * as d3 from "d3";
-// import * as topojson from "topojson";
-import states from "./us-states.json";
+import * as topojson from "topojson";
+import states from "./json/us-states.json";
+import california from "./json/states/ca.json";
+import test from "./json/test.json";
+import test2 from "./json/test2.json";
 import "./App.css";
+
+console.log(states);
+console.log(california);
+
+class USCounty extends React.Component {
+  componentDidMount() {
+    const svg = d3.select(this.anchor);
+    const { width, height, selectedState } = this.props;
+    const projection = d3
+      .geoAlbersUsa()
+      .scale(1280)
+      .translate([width / 2, height / 2]);
+    // const projection = d3
+    //   .geoMercator()
+    //   .center([-120, 37])
+    //   .translate([width / 2, height / 2])
+    //   .scale([width * 3.3]);
+    const path = d3.geoPath(projection);
+
+    const state = states.features.filter(
+      s => s.properties.name === selectedState
+    );
+
+    // var states = topojson.feature(test2, test2.objects.states),
+    //   state = states.features.filter(function(d) {
+    //     return d.id === 6;
+    //   })[0];
+
+    console.log(states, state);
+
+    svg
+      .selectAll("path")
+      .data(state)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("stroke", "#fff")
+      .style("stroke-width", "1")
+      .style("fill", "#00b9ad")
+      .on("click", d => console.log(d));
+  }
+
+  render() {
+    return <g ref={anchor => (this.anchor = anchor)} />;
+  }
+}
 
 class USMap extends React.Component {
   componentDidMount() {
     const svg = d3.select(this.anchor);
     const { width, height, setSelectedState } = this.props;
-
     const projection = d3
       .geoAlbersUsa()
       .scale(1280)
       .translate([width / 2, height / 2]);
-
     const path = d3.geoPath(projection);
 
     svg
@@ -46,17 +93,24 @@ class App extends React.Component {
   };
 
   render() {
-    console.log(this.state.selectedState);
+    const { selectedState } = this.state;
+    console.log(selectedState);
 
     return (
       <div className="map">
         <svg width="960" height="600">
-          <USMap
-            width={960}
-            height={600}
-            setSelectedState={this.setSelectedState}
-          />
+          {selectedState ? (
+            <USCounty width={960} height={600} selectedState={selectedState} />
+          ) : (
+            <USMap
+              width={960}
+              height={600}
+              setSelectedState={this.setSelectedState}
+            />
+          )}
         </svg>
+
+        <button onClick={() => this.setSelectedState(null)}>Back</button>
       </div>
     );
   }
